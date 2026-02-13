7a279422b5b8e031e56f3a932eba1247c1148119
marceru <mcfilho2002@gmail.com>
2026-02-13 20:18:02 -0300

    ajuste ambiente prod e dev

---
+++ meu_modulo_fiscal/models/pos_order.py
@@ -0,0 +1,174 @@
+import json
+import requests  # usado pra enviar requisicoes ao middleware
+from odoo import models, api, fields
+import logging
+import os
+
+
+_logger = logging.getLogger(__name__)
+
+BASE_URL = os.environ.get('MIDDLEWARE_URL', 'http://127.0.0.1:8000')
+# url e token middleware fiscal
+API_LARAVEL_URL = f"{BASE_URL}/api/odoo/webhook"
+API_TOKEN = "123"
+
+
+
+
+class PosOrder(models.Model):
+
+    _inherit = 'pos.order'
+
+  
+
+    def action_pos_order_paid(self):
+       
+        
+        res = super(PosOrder, self).action_pos_order_paid()
+
+
+        try:
+            # estrutura de pagamento
+
+            pagamentos = []
+
+            for pagamento in self.payment_ids:
+                pagamentos.append({
+                    'tipo': pagamento.payment_method_id.name,
+                    'valor': pagamento.amount,
+                })
+
+           # estrutura de produtos com informacoes fiscais
+            dados_dos_produtos = []
+            numero_item_contador = 1 
+
+            for line in self.lines:
+                product = line.product_id
+                
+                # calculo de valores
+                valor_unitario = line.price_unit 
+                quantidade = line.qty
+                valor_bruto_item = valor_unitario * quantidade
+                valor_liquido_item = line.price_subtotal_incl 
+                
+                # calculo do desconto
+
+                valor_desconto_monetario = valor_bruto_item - valor_liquido_item
+                if valor_desconto_monetario < 0.01:
+                    valor_desconto_monetario = 0.0
+
+                item_dict = {
+                    'numero_item': numero_item_contador,
+                    'codigo_produto': product.default_code or str(product.id),
+                    'descricao': product.name,
+                    'codigo_barras': product.barcode or 'SEM GTIN', #todo arrumar codigo de barras
+                    'codigo_ncm': product.x_ncm_id.code, 
+                    'cfop': product.x_cfop, 
+                    'quantidade_comercial': quantidade,
+                    'quantidade_tributavel': quantidade,
+                    'unidade_comercial': line.product_uom_id.name,
+                    'unidade_tributavel': line.product_uom_id.name,
+                    'valor_unitario_comercial': valor_unitario,
+                    'valor_unitario_tributavel': valor_unitario,
+                    'valor_bruto': valor_bruto_item, 
+    
+                    # Impostos
+                    'icms_origem': product.x_origem,
+                    'icms_situacao_tributaria': product.x_icms,
+                    'pis_situacao_tributaria': product.x_pis,
+                    'cofins_situacao_tributaria': product.x_cofins,
+                }
+                if valor_desconto_monetario > 0:
+                    item_dict['valor_desconto'] = valor_desconto_monetario
+
+                dados_dos_produtos.append(item_dict)
+                numero_item_contador += 1
+            
+            # payload final para middleware
+            payload_completo = {
+                'venda': {
+                    'id_odoo': self.name,
+                    'data': self.date_order,
+                    'total': self.amount_total,
+                    'numero_caixa': self.user_id.name, #operador
+                    'numero_ordem': self.pos_reference,
+                },
+                'cliente': {
+                    'nome': 'CONSUMIDOR FINAL',
+                    'cpf': self.x_cpf_nota,
+                    'email': self.x_email_cliente,
+                },
+                'produtos': dados_dos_produtos,
+                'pagamentos': pagamentos,
+                'fiscal': {
+                    'estado': 'AM',
+                    'modelo': '65',
+                    
+                },
+                'confirmacao_venda': bool(self.x_confirmacao_venda), 
+            }
+
+
+            # transforma o payload em json pra enviar pro middleware
+            json_payload = json.dumps(payload_completo, default=str)
+            
+            _logger.info(f"Payload fiscal: {payload_completo} ---")
+
+
+
+
+            
+            # Envio para middleware
+            headers = {
+                 'Content-Type': 'application/json',
+                 'Authorization': f'Bearer {API_TOKEN}',
+                 'Accept': 'application/json'
+            }
+
+            response = requests.post(API_LARAVEL_URL, data=json_payload, headers=headers, timeout=5)
+
+            if response.status_code >= 200 and response.status_code < 300:
+                _logger.info(f"API SUCESSO para {self.name}. Resposta: {response.text}")
+            else:
+                _logger.warning(f"API ERRO para {self.name}. Status: {response.status_code}, Resposta: {response.text}")
+                
+
+        except requests.exceptions.Timeout:
+            _logger.error(f"FALHA API (TIMEOUT) para {self.name}. A venda FOI concluída, mas o JSON não foi enviado.")
+            
+        except requests.exceptions.RequestException as e:
+            _logger.error(f"FALHA API (GERAL) para {self.name}: {e}. A venda FOI concluída, mas o JSON não foi enviado.")
+            
+        except Exception as e:
+            # pega qualquer outro erro
+            _logger.error(f"ERRO INESPERADO ao processar {self.name}: {e}. A venda PODE ter sido concluída, mas o JSON falhou.")
+        
+        return res
+    
+
+
+
+# colocar oq vem do middleware na nota do odoo
+    class PosSession(models.Model):
+        _inherit = 'pos.session'
+
+        def _loader_params_pos_order(self):
+            params = super(PosSession, self)._loader_params_pos_order()
+            
+            # campos retornados pelo middleware
+            params['search_params']['fields'].extend([
+                'x_fiscal_status',
+                'x_fiscal_mensagem',
+                'x_fiscal_chave',
+                'x_fiscal_qrcode_url',
+                'x_fiscal_url_consulta',
+                'x_fiscal_offline', #na tabela ta como boolean false default
+                'x_fiscal_numero',
+                'x_fiscal_serie',
+                'x_fiscal_protocolo',
+                'x_fiscal_qrcode_b64',
+                'x_cpf_nota',  
+                'x_email_cliente',  
+            ])
+            
+            return params
